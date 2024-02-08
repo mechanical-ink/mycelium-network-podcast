e770cdce779817c1053089a8f8134e0896aeb7e0
Schalk Neethling <schalk@mechanical.ink>
2024-02-08 16:16:44 +0200

    add netlify badge and finish configuration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # The Mycelium Network Podcast
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/6d6ee3aa-e867-48d8-b6b1-7656de5165fa/deploy-status)](https://app.netlify.com/sites/myceliumnetworkpodcast/deploys)
+
 This repository is used to plan and track work related to the production of the Mycelium Network podcast.
--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
       colorSecondaryColor: "#dab97e",
       colorSecondaryColorDark: "#38290d",
       colorSecondaryColorLight: "#fff1d7",
+      // <<-- START :: Your elevator pitch
       elevatorPitch:
         "The Mycelium Network podcast. A podcast about early-stage web developers and the mentors, teachers, and communities who help them along the way.",
       // <<-- START :: Featured episode configuration
@@ -41,74 +42,59 @@ export default defineConfig({
         {
           icon: "default-amazon",
           name: "Amazon Music",
-          url: "https://music.amazon.com/podcasts/...",
+          url: "https://music.amazon.com/podcasts/1e69dd02-3b55-4f5a-aa4d-a6ae58a18617/the-mycelium-network-podcast",
         },
         {
           icon: "default-apple",
           name: "Apple Podcasts",
-          url: "https://podcasts.apple.com/...",
+          url: "https://podcasts.apple.com/us/podcast/the-mycelium-network-podcast/id1639357086",
         },
         {
           icon: "default-overcast",
           name: "Overcast",
-          url: "https://overcast.fm/...",
+          url: "https://overcast.fm/itunes1639357086/the-mycelium-network-podcast",
         },
         {
           icon: "default-spotify",
           name: "Spotify",
-          url: "https://open.spotify.com/show/...",
+          url: "https://open.spotify.com/show/2LwRauZaoUP2WPqHKq0wKc?si=3916b1a76c814f61",
         },
         {
           icon: "default-youtube",
           name: "YouTube",
-          url: "https://www.youtube.com/...",
+          url: "https://youtube.com/playlist?list=PL8_kr6gR0_x_lXmOPkah53P5D_92o3ue3&si=Tj-N6-iVqS8Q-t0b",
         },
         {
           icon: "default-rss",
           name: "RSS",
-          url: "https://feeds.buzzsprout.com/...",
+          url: "https://api.substack.com/feed/podcast/1194153/s/89951/private/b8320aaf-74fa-4d6e-88df-daab70d7374b.rss",
         },
       ],
       socialMedia: [
         {
-          icon: "default-discord",
-          name: "Discord",
-          url: "https://www.discord.com/...",
-        },
-        {
-          icon: "default-facebook",
-          name: "Facebook",
-          url: "https://www.facebook.com/...",
-        },
-        {
           icon: "default-github",
           name: "GitHub",
-          url: "https://github.com/...",
-        },
-        {
-          icon: "default-instagram",
-          name: "Instagram",
-          url: "https://www.instagram.com/...",
+          url: "https://github.com/mechanical-ink/mycelium-network-podcast/discussions",
         },
         {
           icon: "default-linkedin",
           name: "LinkedIn",
-          url: "https://www.linkedin.com/...",
+          url: "https://www.linkedin.com/company/mechanical-ink/?viewAsMember=true",
         },
         {
           icon: "default-mastodon",
           name: "Mastodon",
-          url: "https://www.hacyderm.com/...",
+          url: "https://hachyderm.io/@schalkneethling",
         },
         {
           icon: "default-slack",
           name: "Slack",
-          url: "https://www.slack.com/...",
+          url: "https://join.slack.com/t/mechanical-ink-group/shared_invite/zt-22yg9uryr-YiNd2tO1E5qrckQldruWsw",
         },
         {
           icon: "default-x",
           name: "Twitter/X",
-          url: "https://twitter.com/...",
+          url: "https://twitter.com/schalkneethling",
         },
       ],
     }),
