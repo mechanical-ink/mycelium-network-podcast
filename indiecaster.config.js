8ccd39fe0197faddefeddfd3c9dfbd3e1327711e
Schalk Neethling <schalk@mechanical.ink>
2024-02-11 01:11:04 +0200

    upstream sync

---
+++ indiecaster.config.js
@@ -0,0 +1,91 @@
+export const indieCasterConfig = {
+  colorPrimaryColor: "#1c1c44",
+  colorPrimaryColorDark: "#090913",
+  colorPrimaryColorLight: "#f6f6ff",
+  colorSecondaryColor: "#dab97e",
+  colorSecondaryColorDark: "#38290d",
+  colorSecondaryColorLight: "#fff1d7",
+  // <<-- START :: Your elevator pitch
+  elevatorPitch:
+    "The Mycelium Network podcast. A podcast about early-stage web developers and the mentors, teachers, and communities who help them along the way.",
+  // <<-- END :: Your elevator pitch
+  // <<-- START :: Featured episode configuration
+  featuredEpisodeGuestName: "Aashni Shah",
+  featuredEpisodeGuestProfilePicture: "aashni-shah",
+  featuredEpisodeTitle:
+    "Technology as a Force for Good: Empowering, Innovating, and Inspiring - Aashni Shah",
+  featuredEpisodeSummary:
+    "Schalk Neethling, the host of the Mycelium Network podcast, engaged in an inspiring conversation with guest Aashni Shah. Aashni, with her roots in Kenya, has carved a remarkable path in the tech world, starting from her educational journey in Canada to her roles in companies like Microsoft and Square, and her dive into entrepreneurship. A significant part of the discussion revolved around HypeDocs, Aashni's innovative platform designed to enable individuals and employees in companies to document and celebrate their professional achievements. This platform, pivoted to a B2B model, addresses the crucial need for self-promotion and recognition in the workplace, particularly highlighting the challenges faced by underrepresented groups in the tech industry.",
+  featuredEpisodeTrack:
+    "technology-as-a-force-for-good-empowering-innovating-and-inspiring-aashni-shah",
+  featuredEpisodeURL:
+    "technology-as-a-force-for-good-empowering-innovating-and-inspiring-aashni-shah",
+  // <<-- END :: Featured episode configuration
+  headerBackgroundColor: "#1c1c44",
+  headerForegroundColor: "#fff",
+  hostName: "Schalk Neethling",
+  hostProfilePicture: "schalk-neethling",
+  logo: "logo.svg",
+  logoHeight: "275",
+  logoWidth: "500",
+  podcastName: "The Mycelium Network Podcast",
+  podcastPlayers: [
+    {
+      icon: "default-amazon",
+      name: "Amazon Music",
+      url: "https://music.amazon.com/podcasts/1e69dd02-3b55-4f5a-aa4d-a6ae58a18617/the-mycelium-network-podcast",
+    },
+    {
+      icon: "default-apple",
+      name: "Apple Podcasts",
+      url: "https://podcasts.apple.com/us/podcast/the-mycelium-network-podcast/id1639357086",
+    },
+    {
+      icon: "default-overcast",
+      name: "Overcast",
+      url: "https://overcast.fm/itunes1639357086/the-mycelium-network-podcast",
+    },
+    {
+      icon: "default-spotify",
+      name: "Spotify",
+      url: "https://open.spotify.com/show/2LwRauZaoUP2WPqHKq0wKc?si=3916b1a76c814f61",
+    },
+    {
+      icon: "default-youtube",
+      name: "YouTube",
+      url: "https://youtube.com/playlist?list=PL8_kr6gR0_x_lXmOPkah53P5D_92o3ue3&si=Tj-N6-iVqS8Q-t0b",
+    },
+    {
+      icon: "default-rss",
+      name: "RSS",
+      url: "https://api.substack.com/feed/podcast/1194153/s/89951/private/b8320aaf-74fa-4d6e-88df-daab70d7374b.rss",
+    },
+  ],
+  socialMedia: [
+    {
+      icon: "default-github",
+      name: "GitHub",
+      url: "https://github.com/mechanical-ink/mycelium-network-podcast/discussions",
+    },
+    {
+      icon: "default-linkedin",
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/company/mechanical-ink/",
+    },
+    {
+      icon: "default-mastodon",
+      name: "Mastodon",
+      url: "https://hachyderm.io/@schalkneethling",
+    },
+    {
+      icon: "default-slack",
+      name: "Slack",
+      url: "https://join.slack.com/t/mechanical-ink-group/shared_invite/zt-22yg9uryr-YiNd2tO1E5qrckQldruWsw",
+    },
+    {
+      icon: "default-x",
+      name: "Twitter/X",
+      url: "https://twitter.com/schalkneethling",
+    },
+  ],
+};
